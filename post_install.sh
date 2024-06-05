448d18a6a2d6e514dae7d05ecf013c43de684d8c
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-05 23:38:23 +0530

    add single script for post install

---
+++ post_install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Initialize sudo to prevent asking for password later
+chsh -s /usr/bin/zsh alpha
+sudo id
+
+# Clone dotfiles repo
+git clone --recursive https://github.com/sudoAlphaX/dotfiles ~/.dotfiles
+
+# Install paru (AUR Helper)
+sudo pacman -S --needed base-devel && mkdir ~/repos && cd ~/repos && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si && cd ~
+
+# Setup fancontrol
+sudo cp ~/.dotfiles/assets/configs/etc/fancontrol /etc/
+sudo systemctl enable --now fancontrol.service
+
+# Install browser
+paru -S --noconfirm --sudoloop librewolf-bin
+
+# Install musicplayers
+paru -S --noconfirm --sudoloop mprisence musikcube spotube-bin
+systemctl --user enable mprisence.service
+
+# Install fonts
+mkdir -p ~/.local/share/fonts
+paru -S --noconfirm --sudoloop ttf-ms-win11-auto
+wget -q -O- https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.tar.xz | tar -Jx -C ~/.local/share/fonts;
+wget -o ~/.local/share/fonts/Pacifico-Regular.ttf "https://github.com/googlefonts/Pacifico/raw/main/fonts/ttf/Pacifico-Regular.ttf"
+cp ~/.dotfiles/.config/rofi/assets/rofi-git/fonts/* ~/.local/share/fonts/
+
+# Install themes
+paru -S catppuccin-gtk-theme-mocha papirus-folders-catppuccin-git
+gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-mauve-standard+default"
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface icon-theme Papirus
+papirus-folders -C cat-mocha-mauve
+
+# Install Hyprland
+paru -S --noconfirm --sudoloop hyprlang 
+paru -S --noconfirm --sudoloop hyprland-git hyprpaper-git hyprlock-git hypridle-git hyprcursor-git xdg-desktop-portal-hyprland-git
+
+# Install other packages
+paru -S --noconfirm --sudoloop auto-cpufreq
+sudo systemctl enable --now auto-cpufreq.service
+sudo systemctl mask power-profiles-daemon.service
+
+# Other tasks
+paru -S --noconfirm --sudoloop wl-clip-persist
+sudo systemctl enable reflector.timer reflector.service pkgfile-update.timer bluetooth.service avahi-daemon.service avahi-dnsconfd.service 
+systemctl enable --user syncthing.service
+cd ~/.dotfiles/assets/utilities/arrpc && npm install && cd ~
+pipx install yt-dlp hanimetv tldr virtualenvwrapper
