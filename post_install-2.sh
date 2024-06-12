4c6d7aa61e64cf8a4a620322932a6d391827ebf7
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-12 22:10:53 +0530

    enable sshd service

--- post_install-2.sh
+++ post_install-2.sh
@@ -12,7 +12,7 @@ sudo systemctl mask power-profiles-daemon.service
 
 # Other tasks
 paru -S --noconfirm --sudoloop wl-clip-persist
-sudo systemctl enable reflector.timer reflector.service pkgfile-update.timer bluetooth.service avahi-daemon.service avahi-dnsconfd.service 
+sudo systemctl enable reflector.timer reflector.service pkgfile-update.timer bluetooth.service avahi-daemon.service avahi-dnsconfd.service sshd.service
 systemctl enable --user syncthing.service
 cd ~/.dotfiles/assets/utilities/arrpc && npm install && cd ~
 pipx install yt-dlp hanimetv tldr virtualenvwrapper
