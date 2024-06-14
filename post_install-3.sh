d9f18b4dd19a419708388881fb033d340d017420
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-14 22:02:58 +0530

    add post_install-3.sh for extra packages

---
+++ post_install-3.sh
@@ -0,0 +1,9 @@
+sudo paru --noconfirm --sudoloop yazi
+sudo paru --noconfirm --sudoloop cpio
+
+sudo paru --noconfirm --sudoloop qemu-full dnsmasq virt-manager
+sudo systemctl enable libvirtd.service
+sudo usermod -G libvirt -a alpha
+sudo usermod -G libvirt-qemu -a alpha
+
+sudo paru -S intellij-idea-community-edition kotlin gradle jdk21-openjdk
