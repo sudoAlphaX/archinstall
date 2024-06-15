01beb4d0eb77633eb305061a4098b08a25fc4870
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-15 08:04:37 +0530

    correct flags (-S) for intellij, gradle, etc install

--- post_install-3.sh
+++ post_install-3.sh
@@ -2,7 +2,7 @@ sudo paru --noconfirm --sudoloop yazi
 
 sudo paru -S --noconfirm --sudoloop qemu-full dnsmasq virt-manager && sudo systemctl enable libvirtd.service && sudo usermod -G libvirt -a $(whoami) && sudo usermod -G libvirt-qemu -a $(whoami)
 
-sudo paru -S intellij-idea-community-edition kotlin gradle jdk21-openjdk
+sudo paru -S --noconfirm --sudoloop intellij-idea-community-edition kotlin gradle jdk21-openjdk
 
 # Install musicplayers
 paru -S --noconfirm --sudoloop musikcube spotube-bin
