69de64cc62d00ad35666a3b72b514376d684f564
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-09 17:38:17 +0530

    remove sudo in paru commands

--- post_install-3.sh
+++ post_install-3.sh
@@ -1,8 +1,8 @@
-sudo paru --noconfirm --sudoloop yazi
+paru --noconfirm --sudoloop yazi
 
-sudo paru -S --noconfirm --sudoloop qemu-full dnsmasq virt-manager && sudo systemctl enable libvirtd.service && sudo usermod -G libvirt -a $(whoami) && sudo usermod -G libvirt-qemu -a $(whoami)
+paru -S --noconfirm --sudoloop qemu-full dnsmasq virt-manager quickemu && sudo systemctl enable libvirtd.service && sudo usermod -G libvirt -a $(whoami) && sudo usermod -G libvirt-qemu -a $(whoami)
 
-sudo paru -S --noconfirm --sudoloop intellij-idea-community-edition kotlin gradle jdk21-openjdk
+paru -S --noconfirm --sudoloop intellij-idea-community-edition kotlin gradle jdk21-openjdk
 
 # Install musicplayers
 paru -S --noconfirm --sudoloop musikcube spotube
