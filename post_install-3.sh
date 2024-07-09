970194ba67ec19b7f57d6733df11535bff83be39
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-09 20:15:44 +0530

    add -S flag to yazi

--- post_install-3.sh
+++ post_install-3.sh
@@ -1,4 +1,4 @@
-paru --noconfirm --sudoloop yazi
+paru -S --noconfirm --sudoloop yazi
 
 paru -S --noconfirm --sudoloop qemu-full dnsmasq virt-manager quickemu && sudo systemctl enable libvirtd.service && sudo usermod -G libvirt -a $(whoami) && sudo usermod -G libvirt-qemu -a $(whoami)
 
