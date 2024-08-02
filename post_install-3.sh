c7698d77322f775c3ad8dd3f0ed3c9adbc948ad9
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-08-02 20:19:40 +0530

    reenable mprisense cause not 𝓯𝓻𝓮𝓪𝓴𝔂
    This commit reverts fcb62c0 cause it is possible to not browser media in
    discord rpc

--- post_install-3.sh
+++ post_install-3.sh
@@ -5,6 +5,7 @@ paru -S --noconfirm --sudoloop qemu-full dnsmasq virt-manager quickemu && sudo s
 paru -S --noconfirm --sudoloop intellij-idea-community-edition kotlin gradle jdk21-openjdk
 
 # Install musicplayers
-paru -S --noconfirm --sudoloop musikcube spotube
+paru -S --noconfirm --sudoloop musikcube spotube mprisence
+systemctl --user enable mprisence.service
 
 paru -S --noconfirm --sudoloop ventoy
