229f4f56b61ae9e451c1c4bac8bedf089358c660
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2025-01-11 20:09:27 +0530

    add dotfiles service

--- userwise.sh
+++ userwise.sh
@@ -8,3 +8,4 @@ sudo usermod -G libvirt-qemu -a "$(whoami)"
 # Systemd user units
 systemctl --user enable mprisence.service
 systemctl --user enable syncthing.service
+systemctl --user enable dotfiles.service
